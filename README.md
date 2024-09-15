313554c01148262f106ad7da76605fd722ed0b6c
aricamargo_meli <dario.camargo@mercadolivre.com>
2024-09-15 18:35:58 -0300

    unidirectional stream

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ $ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest```
 
 4 - Generate proto
 ```protoc --go_out=. --go-grpc_out=. proto/course_category.proto```
+
+5 - Run evans
+```evans -r repl```
+
+6 - Exec:
+```package pb
+service CategoryService
+call <func>
+```
--- internal/service/category.go
+++ internal/service/category.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"context"
+	"errors"
+	"io"
 
 	"github.com/dpcamargo/fullcycle-grpc/internal/database"
 	"github.com/dpcamargo/fullcycle-grpc/internal/pb"
@@ -66,4 +68,28 @@ func (c *CategoryService) GetCategory(_ context.Context, in *pb.CategoryGetReque
 		Name:        category.Name,
 		Description: category.Description,
 	}, nil
+}
+
+func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error {
+	categories := &pb.CategoryList{}
+
+	for {
+		category, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			return stream.SendAndClose(categories)
+		}
+		if err != nil {
+			return err
+		}
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+
+		categories.Categories = append(categories.Categories, &pb.Category{
+			Id:          categoryResult.ID,
+			Name:        categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+	}
 }
